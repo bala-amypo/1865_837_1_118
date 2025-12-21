4354ce9abb7b74aed9423adaebe47af0d5e43ca3
bala-amypo <bala@amypo.in>
2025-12-21 15:27:44 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -1,17 +1,10 @@
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-[cite_start]public class ApiResponse { // [cite: 871]
-    private LocalDateTime timestamp;
-    private int status;
-    private String error;
+public class ApiResponse {
     private String message;
-    private String path;
+    
+    public ApiResponse() {}
 }
