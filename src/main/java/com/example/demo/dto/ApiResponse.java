d56ac2699f638555b8f94bdddb706d7a30991026
bala-amypo <bala@amypo.in>
2025-12-21 16:37:27 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -1,17 +1,18 @@
 package com.example.demo.dto;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-[cite_start]public class ApiResponse { // [cite: 871]
-    private LocalDateTime timestamp;
-    private int status;
-    private String error;
+public class ApiResponse {
     private String message;
-    private String path;
+    private Boolean success;
+    private Object data;
+
+    public ApiResponse(String message, Boolean success) {
+        this.message = message;
+        this.success = success;
+    }
 }
--- src/main/java/com/example/demo/repository/AppUserRepository.java
+++ src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -1,5 +1,4 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
