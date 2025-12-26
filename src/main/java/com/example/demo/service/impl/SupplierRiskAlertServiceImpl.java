ca8b3112d2faa3b68f6aaf2d94e1c2670b98c77a
bala-amypo <bala@amypo.in>
2025-12-26 18:59:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplierRiskAlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplierRiskAlertServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.SupplierRiskAlert;
 import com.example.demo.repository.SupplierRiskAlertRepository;
 import com.example.demo.service.SupplierRiskAlertService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public class SupplierRiskAlertServiceImpl implements SupplierRiskAlertService {
     private final SupplierRiskAlertRepository repository;
 
+    @Autowired
     public SupplierRiskAlertServiceImpl(SupplierRiskAlertRepository repository) {
         this.repository = repository;
     }
