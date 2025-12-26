28532031fdd33a7dbfeee8c0842eae59a4f1c69d
bala-amypo <bala@amypo.in>
2025-12-26 16:41:37 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -1,36 +1,27 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "supplier_profile")
+@Data
+@NoArgsConstructor
+@Table(name = "supplier_profiles")
 public class SupplierProfile {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(unique = true)
     private String supplierCode;
 
+    @NotBlank
     private String supplierName;
 
     private String email;
-
+    private String phone;
     private Boolean active = true;
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getSupplierCode() { return supplierCode; }
-    public void setSupplierCode(String supplierCode) { this.supplierCode = supplierCode; }
-
-    public String getSupplierName() { return supplierName; }
-    public void setSupplierName(String supplierName) { this.supplierName = supplierName; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
 }
