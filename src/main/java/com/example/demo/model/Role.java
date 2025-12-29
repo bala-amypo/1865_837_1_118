6fe28a3ce04c7127b967b18bd225c359873c708d
bala-amypo <bala@amypo.in>
2025-12-29 15:28:12 +0000

    User commit

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -1,10 +1,7 @@
 package com.example.demo.model;
 
 public enum Role {
-    // If the test expects the full qualified name:
-    ROLE_USER,
-    ROLE_ADMIN, 
-    ROLE_SUPPLIER
-    
-    // If you currently have just USER, ADMIN, SUPPLIER, change them above.
+    ADMIN,
+    MANAGER,
+    ANALYST
 }
